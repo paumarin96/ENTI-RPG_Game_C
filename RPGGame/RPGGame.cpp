3869d3dec46f6cb9bb09c18e9d97a67613edaa4f
Focalita <75481078+Focalita@users.noreply.github.com>
2020-12-05 12:14:55 +0100

    hola
    aloh

--- RPGGame/RPGGame.cpp
+++ RPGGame/RPGGame.cpp
@@ -1,10 +1,31 @@
 // RPGGame.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
+#include <cassert>
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hola !\n";
+using namespace std;
+
+int main() {
+    unsigned lines = 0;
+    string line_content;
+
+    ifstream r_tftpd_hpa("radev.txt");
+    assert(r_tftpd_hpa);
+
+    while (getline(r_tftpd_hpa, line_content)) {
+        lines++;
+
+        if (line_content[0] == ' ') { // my failed attempt at catching spaces
+            cout << endl << " " << endl;
+           
+        }
+
+        cout << line_content ;
+       
+    }
+
+    return 0;
 }
-
